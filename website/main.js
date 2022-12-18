1949e5e797a87b4f6981a6139beeb82f423d20b4
adamejhonrey <95111969+adamejhonrey@users.noreply.github.com>
2022-12-18 22:38:42 +0800

    Update main.js

--- website/main.js
+++ website/main.js
@@ -39,3 +39,82 @@ downloadButton.addEventListener('click', () => {
   a.download = 'index.html';
   a.click();
 });
+
+// Customization options for each element
+const form = document.createElement('form');
+form.innerHTML = '<label for="element-content">Content:</label><br><textarea id="element-content" name="element-content"></textarea><br><button type="submit">Update</button>';
+form.style.display = 'none';
+document.body.appendChild(form);
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const content = document.getElementById('element-content').value;
+  selectedElement.innerHTML = content;
+  form.style.display = 'none';
+});
+
+// Add click event listener to the canvas
+canvas.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) {
+    // Display the customization form for the selected element
+    selectedElement = e.target;
+    document.getElementById('element-content').value = selectedElement.innerHTML;
+    form.style.display = 'block';
+  }
+});
+
+// Responsive layout
+const breakpoint = 600; // Adjust this value as needed
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > breakpoint) {
+    // Display tools and canvas side by side
+    toolbox.style.display = 'block';
+    canvas.style.width = 'calc(100% - 200px)';
+  } else {
+    // Display tools and canvas one above the other
+    toolbox.style.display = 'none';
+    canvas.style.width = '100%';
+  }
+});
+
+// Page navigation
+const nav = document.createElement('nav');
+nav.innerHTML = '<a href="#">Home</a> <a href="#">About</a> <a href="#">Contact</a>';
+document.body.insertBefore(nav, canvas);
+
+// File upload
+const fileInput = document.createElement('input');
+fileInput.type = 'file';
+fileInput.accept = 'image/*';
+fileInput.style.display = 'none';
+document.body.appendChild(fileInput);
+
+fileInput.addEventListener('change', () => {
+  // Use the selected file to update an image element on the canvas
+  const file = fileInput.files[0];
+  const reader = new FileReader();
+  reader.addEventListener('load', () => {
+    selectedElement.src = reader.result;
+  });
+  reader.readAsDataURL(file);
+});
+
+// Saving and loading
+const saveButton = document.createElement('button');
+saveButton.innerHTML = 'Save';
+document.body.insertBefore(saveButton, nav);
+
+saveButton.addEventListener('click', () => {
+  // Save the current state of the canvas to local storage
+  localStorage.setItem('canvas', canvas.innerHTML);
+});
+
+const loadButton = document.createElement('button');
+loadButton.innerHTML = 'Load';
+document.body.insertBefore(loadButton, nav);
+
+loadButton.addEventListener('click', () => {
+  // Load the saved state of the canvas from local storage
+  canvas.innerHTML = localStorage.getItem('canvas');
+});
