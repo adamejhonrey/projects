a53a4a571d3c0066b69c5f180adec786cb549894
adamejhonrey <95111969+adamejhonrey@users.noreply.github.com>
2022-12-18 22:08:39 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,40 @@
+// Get all the nav links
+const navLinks = document.querySelectorAll('header nav a');
+
+// Add a click event listener to each nav link
+navLinks.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+
+    // Get the target element from the link's href
+    const target = document.querySelector(e.target.getAttribute('href'));
+
+    // Animate the scroll to the target element
+    smoothScroll(target, 500);
+  });
+});
+
+// Smooth scroll function
+function smoothScroll(target, duration) {
+  let targetPosition = target.getBoundingClientRect().top;
+  let startPosition = window.pageYOffset;
+  let distance = targetPosition - startPosition;
+  let startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    let timeElapsed = currentTime - startTime;
+    let run = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
